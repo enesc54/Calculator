3f95376676f068500f9d8c516f23c401de87a051
Enes CAN <131619156+enesc54@users.noreply.github.com>
2023-04-25 18:45:57 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+let display = document.querySelector(".display")
+isCalculated = false
+let operation = ''
+isNumber = true
+
+function displayChanged(){
+    console.log("changed")
+}
+
+function insert(sender){
+    if (sender.className.includes("numbers")){
+        if (isCalculated){
+            display.textContent = ""
+            operation = ''
+            isCalculated = false
+        }
+        isNumber = true
+    }else if (isNumber){
+        isCalculated = false
+        isNumber = false
+    }else{
+        del()
+        isCalculated = false
+    }
+    display.textContent += sender.textContent
+    operation += sender.id
+}
+
+function del(){
+    operation = operation.slice(0,operation.length - 1)
+    display.textContent = display.textContent.slice(0, display.textContent.length - 1)
+}
+
+function clr(){
+    operation = ""
+    display.textContent = ""
+    isCalculated = false
+}
+
+function calc(){
+    display.textContent = eval(operation)
+    operation = eval(operation)
+    isCalculated = true
+}
